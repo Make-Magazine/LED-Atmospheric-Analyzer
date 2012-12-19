eaa0adc1c5050af99e4714229bef3e7536e50206
Eric Weinhoffer <eweinhoffer@gmail.com>
2012-12-19 11:04:51 -0800

    Changed some comments and removed some code.
    Code removed wasn't helpful for this purpose: getting up and running
    with the Seeed LCD and LEDs. Patrick's code in his book is more
    comprehensive and includes data storage.

--- LED_Photometer_Seeed_LCD/LED_Photometer_Seeed_LCD.ino
+++ LED_Photometer_Seeed_LCD/LED_Photometer_Seeed_LCD.ino
@@ -7,8 +7,10 @@
   
   This code is based on Patrick's LED Photometer project from the book "Atmospheric Monitoring with Arduino".
   You can find his code and a schematic on page 54. I modified it for use with the Seeed LCD screen, and left out data storage.
+  Patrick's code is much more comprehensive; this test code is meant to get you up and running with the Seeed LCD.
+  
   The LCD must be powered from the 5V pin on the Arduino, with TX hooked up to Digital Pin 3 and RX hooked up to Digital Pin 5.
-  These pins can be changed on Line 27.
+  These pins can be changed on Line 29.
 
 */
 
@@ -21,48 +23,39 @@
 
 
 int redPin = A1;    
-int greenPin =A2;
-int bluePin=A3;
+int greenPin = A2;
+int bluePin= A3;
 
 SerialLCD slcd(3,5); // Assign pins the LCD is connected to: slcd(TX,RX)
 
 // A place to store the values coming from the analog port
-int sensorValueRed =0;  
-int sensorValueGreen =0;  
+int sensorValueRed = 0;  
+int sensorValueGreen = 0;  
 int sensorValueBlue = 0;
 
 //A place to store the maximum value for each LED
 int maxRed = 0;
-int maxGreen =0;
+int maxGreen = 0;
 int maxBlue = 0;
 
 
-int record=0;
+int record= 0;
 int reclen = 7;
-int addr =0; 
+int addr = 0; 
 
 
 long timeSinceLastSensorHigh = 0;
 int dataWritten = 0;
 
 // music section
-int notelen =40;
-int dlx = notelen *1.33;
+int notelen = 40;
+int dlx = notelen * 1.33;
 
 
 void setup() 
 {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
-  
-  // Set the Analog Pins
-  // Why are we setting input pins to output? 
-  // We're doing this to prevent "leakage" from the pins.
-  // Setting the pins to output activates a pullup resistor,
-  // which makes it difficult for voltage to come into the Arduino,
-  // until we're ready for it.
-
-  Serial.println("Setting up the Analog Pins");
+  Serial.begin(9600); // initialize serial communications at 9600 bps
+  slcd.begin(); // initialize the LCD
   
   pinMode(redPin, OUTPUT);
   digitalWrite(redPin, LOW);
@@ -70,9 +63,6 @@ void setup()
   digitalWrite(greenPin, LOW);
   pinMode(bluePin, OUTPUT); 
   digitalWrite(bluePin, LOW);
-
-  slcd.begin();
-
 
 // Play music to let user know the gadget is ready
   tone(7,294,notelen);
@@ -92,18 +82,12 @@ void setup()
 void loop() 
 {
   // read the value from the sensor:
-  /* 
-    Back in setup(), we enabled a pullup resistor on the analog pins, which
-    made it difficult for electricity to come into the analog pins.
-    Here, we disable the pullup resistor, wait 10ms for the pin to stabilize,
-    read the voltage coming into the pin, then reenable the pullup resistor. 
-  */
   slcd.clear();
   pinMode(redPin, INPUT);
   delay(10);
   slcd.setCursor(0, 0);
   slcd.print("Reading red:  ");
-  sensorValueRed= analogRead(redPin);
+  sensorValueRed = analogRead(redPin);
   pinMode(redPin, OUTPUT);  
   slcd.print(sensorValueRed,DEC);
   delay(3000);
@@ -121,7 +105,7 @@ void loop()
   slcd.clear();
   pinMode(bluePin, INPUT);
   delay(10);
-  slcd.setCursor(0,0);
+  slcd.setCursor(0, 0);
   slcd.print("Reading blue: ");
   sensorValueBlue = analogRead(bluePin);
   pinMode(bluePin, OUTPUT);
@@ -146,6 +130,4 @@ void loop()
   if(sensorValueRed>maxRed) maxRed = sensorValueRed;
   if(sensorValueGreen>maxGreen) maxGreen = sensorValueGreen;
   if(sensorValueBlue>maxBlue) maxBlue = sensorValueBlue;
-
-  // Display the sensor values on the LCD screen
 }
